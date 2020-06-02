120fc693b65531230b19b74a7bdf749e1bba9e13
dadang <hey.dadangkurniawan@gmail.com>
2020-06-02 18:49:58 +0700

    add font awesome

--- src/components/cart/CartItem.vue
+++ src/components/cart/CartItem.vue
@@ -1,7 +1,7 @@
 <template>
     <li class="list-group-item d-flex justify-content-between align-items-center" id="summary_product_' + id +'">
         <span id="summary_product_name_' + id + '">Air Jordan  Noble Red x 1</span> 
-        <span class="btn btn-sm btn-secondary" ><i class="fa fa-trash"></i></span>
+        <span class="btn btn-sm btn-secondary" ><font-awesome-icon icon="trash" /></span>
     </li>
 </template>
 <script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue';
 import App from './App.vue';
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faUserSecret, faTrash)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 Vue.config.productionTip = false;
 
