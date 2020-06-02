c5a44308f0193b5cb629f9391f6c8296c2412612
dadang <hey.dadangkurniawan@gmail.com>
2020-06-02 18:01:37 +0700

    init commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render: h => h(App)
+}).$mount('#app');
